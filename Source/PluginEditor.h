ca946beff24f94befc4eebb2c19bf3915a627c6d
Abid A <abraar@Abraars-Air.communityfibre.co.uk>
2024-11-17 09:00:53 +0000

    Add Initial Project Build

---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class Simple_OscillatorAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    Simple_OscillatorAudioProcessorEditor (Simple_OscillatorAudioProcessor&);
+    ~Simple_OscillatorAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    Simple_OscillatorAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Simple_OscillatorAudioProcessorEditor)
+};
